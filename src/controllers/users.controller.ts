1f966e23cf5fa52e4cd8fd23574aa2cba7cff768
expel <connoroakvillea32@hotmail.com>
2025-03-10 19:19:45 -0400

    PLEASE

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,28 +1,10 @@
 import { Request, Response } from 'express';
 import { createHash } from 'crypto';
 
-const VERIFICATION_TOKEN = process.env.EBAY_VERIFICATION_TOKEN || '';  // Make sure to set this in your .env
-
 export const AccountDeletion = (req: Request, res: Response) => { 
-  console.log("Received eBay account deletion notification:", req.body);
-  
-  if (req.body.challenge_code) {
-    const hash = createHash('sha256');
-    hash.update(req.body.challenge_code);
-    hash.update(VERIFICATION_TOKEN);
-    hash.update(req.path);
-    const responseHash = hash.digest('hex');
-    res.status(200).json({ challengeResponse: responseHash });
-    return 
-  }
-  
-  res.status(200).json({ message: "ok" });
-  return;
-}
-
-export const AccountDeletion2 = (req: Request, res: Response) => { 
   console.log("Received eBay account deletion notification:", req.query);
-  
+  const VERIFICATION_TOKEN = process.env.EBAY_VERIFICATION_TOKEN || '';  // Make sure to set this in your .env
+  console.log('hello;', VERIFICATION_TOKEN);
   if (req.query.challenge_code) {
     const hash = createHash('sha256');
     hash.update(req.query.challenge_code as string);
@@ -32,7 +14,6 @@ export const AccountDeletion2 = (req: Request, res: Response) => {
     res.status(200).json({ challengeResponse: responseHash });
     return 
   }
-
   res.status(200).json({ message: "ok" });
   return;
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import type { RequestHandler } from 'express';
-import { AccountDeletion, AccountDeletion2 } from '../controllers/users.controller';
+import { AccountDeletion } from '../controllers/users.controller';
 
 const router = Router();
 
-router.post("/ebay-account-deletion", AccountDeletion);
-router.get("/ebay-account-deletion", AccountDeletion2);
+router.get("/ebay-account-deletion", AccountDeletion);
 
 export default router;
